601d6e2fc623614687fbce80840eeaa4b515cccd
canoooon570 <caulrickchurchill@gmail.com>
2026-01-01 23:59:11 +0100

    Add client and backend

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("connected to MongoDB");
+}).catch((err) => {
+  console.log("Error: " + err)
+});
+
+
+
+
+
+
+const app = express()
+const port = 8000
+
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
+})
